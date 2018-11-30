1a0020394ab1aab1448c72d0ba5ab6ff267dbc94
MrGaabriel <hello@mrgaabriel.space>
2018-11-30 13:23:51 -0200

    Também procurar bans pelo IP

--- src/main/kotlin/net/perfectdreams/dreamnetworkbans/commands/UnbanCommand.kt
+++ src/main/kotlin/net/perfectdreams/dreamnetworkbans/commands/UnbanCommand.kt
@@ -8,8 +8,12 @@ import net.perfectdreams.dreamcorebungee.utils.extensions.toTextComponent
 import net.perfectdreams.dreamnetworkbans.DreamNetworkBans
 import net.perfectdreams.dreamnetworkbans.PunishmentManager
 import net.perfectdreams.dreamnetworkbans.dao.Ban
+import net.perfectdreams.dreamnetworkbans.dao.GeoLocalization
 import net.perfectdreams.dreamnetworkbans.tables.Bans
+import net.perfectdreams.dreamnetworkbans.tables.GeoLocalizations
+import net.perfectdreams.dreamnetworkbans.tables.IpBans
 import net.perfectdreams.libs.com.mongodb.client.model.Filters
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.lang.IllegalArgumentException
@@ -19,9 +23,18 @@ class UnbanCommand(val m: DreamNetworkBans) : AbstractCommand("unban", permissio
 	@Subcommand
     fun unban(sender: CommandSender, playerName: String) {
 		val punishedUniqueId = try { UUID.fromString(playerName) } catch (e: IllegalArgumentException) { PunishmentManager.getUniqueId(playerName) }
-
+		
+		val geoLocalization = transaction(Databases.databaseNetwork) {
+			GeoLocalization.find { GeoLocalizations.player eq punishedUniqueId!! }.firstOrNull()
+		}
+		
+		val ip = geoLocalization?.ip
+		
 		transaction(Databases.databaseNetwork) {
-			Ban.find { Bans.player eq punishedUniqueId }.firstOrNull()?.delete()
+			Bans.deleteWhere { Bans.player eq punishedUniqueId }
+			
+			if (ip != null)
+				IpBans.deleteWhere { IpBans.ip eq ip }
 		}
 
 		sender.sendMessage("§b$punishedUniqueId§a desbanido com sucesso!".toTextComponent())
