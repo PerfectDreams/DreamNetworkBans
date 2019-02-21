c3d4de104abb02764da7a5ac16a48e20ccac78b2
MrGaabriel <hello@mrgaabriel.space>
2019-02-20 22:45:54 -0300

    Aumentar a duração do ipban padrão

--- src/main/kotlin/net/perfectdreams/dreamnetworkbans/PunishmentManager.kt
+++ src/main/kotlin/net/perfectdreams/dreamnetworkbans/PunishmentManager.kt
@@ -6,8 +6,8 @@ object PunishmentManager {
 	// 14 dias
 	const val WARN_EXPIRATION = 1_209_600_000
 	
-	// 7 dias
-	const val DEFAULT_IPBAN_EXPIRATION = (WARN_EXPIRATION / 2).toLong()
+	// 90 dias
+	const val DEFAULT_IPBAN_EXPIRATION = 466_560_000_000
 	
 	fun getUniqueId(playerName: String): UUID {
 		// UUIDs podem ser diferentes... mas já que a gente é um offline mode boi, complicar pra quê?
